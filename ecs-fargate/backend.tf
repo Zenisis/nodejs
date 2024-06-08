9a912c2d66e6b3c495d37922914c839fefe25ff0
Zenisis <119743135+Zenisis@users.noreply.github.com>
2024-06-08 17:51:04 +0530

    Update backend.tf

--- ecs-fargate/backend.tf
+++ ecs-fargate/backend.tf
@@ -2,7 +2,7 @@ terraform {
 
 
   backend "s3" {
-    bucket = "new2311" # Replace with your actual S3 bucket name
+    bucket = "nx1" # Replace with your actual S3 bucket name
     key    = "nodejs/ecs-fargate/terraform.tfstate"
     region = "eu-north-1"
   }
