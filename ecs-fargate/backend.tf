36916516a153d9d5c255b57bd1db48041f7482b0
zenisis <zenisis@kartik.>
2024-06-08 17:43:20 +0530

    change2

---
+++ ecs-fargate/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+
+
+  backend "s3" {
+    bucket = "new2311" # Replace with your actual S3 bucket name
+    key    = "nodejs/ecs-fargate/terraform.tfstate"
+    region = "us-east-1"
+  }
+
+
+}
