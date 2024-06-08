f0ea00e70b7275356e6a57b4a202a3c906cdb84a
Zenisis <119743135+Zenisis@users.noreply.github.com>
2024-06-08 23:02:26 +0530

    Update output.tf

--- ecs-module/output.tf
+++ ecs-module/output.tf
@@ -1,14 +1,4 @@
-output "cluster_id" {
-  value = aws_ecs_cluster.main.id
-}
 
-output "service_id" {
-  value = aws_ecs_service.app1.id
-}
-
-output "task_definition_arn" {
-  value = aws_ecs_task_definition.td.arn
-}
 
 output "lb_dns_name" {
   value = aws_lb.lb.dns_name
